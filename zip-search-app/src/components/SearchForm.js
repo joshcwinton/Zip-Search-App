1086e7d74402dcea0c9a9c5837326f534715a2d3
Belinda Liu <belinda.liu@rocketmail.com>
2019-06-05 15:26:18 -0400

    new functions

--- zip-search-app/src/components/SearchForm.js
+++ zip-search-app/src/components/SearchForm.js
@@ -2,23 +2,43 @@ import React, { Component } from 'react';
 import './SearchForm.css';
 import axios from 'axios';
 
+
 class SearchForm extends Component {
   constructor(props){
     super(props);
     this.state = {
-
     }
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+
+
+  handleSubmit = async(event) =>
+  {
+     if (event.key === 'Enter')
+     {
+        let zipURI = 'http://ctp-zip-api.herokuapp.com/zip/';
+        let zip = event.target.value;
+        zipURI = zipURI + zip;
+
+        await axios.get(zipURI)
+        .then(response =>
+          {
+             console.log(response);
+          });
+      }
   }
 
   render(){
     return (
       <div id='search-form'>
         <form>
-          Zip Code:  <input type="text" name="zip" placeholder="Try 10016" value={this.state.zipCode}/>
+          Zip Code:  <input type="text" name="zip" placeholder="Try 10016" value={this.state.zipCode} onKeyDown= {this.handleSubmit}/>
         </form>
       </div>
     );
-  }
+    }
+
 }
 
 export default SearchForm;
