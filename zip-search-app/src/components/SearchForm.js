34e4a333127517810c08d5865431809b92bd94c0
Tommi-Tsuruga <tommi.tsuruga@gmail.com>
2019-06-06 12:08:45 -0400

    creates arrays, returns them, cards need formatting

--- zip-search-app/src/components/SearchForm.js
+++ zip-search-app/src/components/SearchForm.js
@@ -19,7 +19,9 @@ const getCity = async (zipCode) => {
 class SearchForm extends Component {
   constructor(props) {
     super(props);
+    //const cityList = [];
     this.state = {
+      cityList: [],
       zipCode: this.props.zipCode,
       city: this.props.city,
       state: this.props.state,
@@ -44,19 +46,30 @@ class SearchForm extends Component {
 
   zipSearch = async (zipCode) => {
     console.log("zip", zipCode)
-    let temp = zipCode
-    let response = await getCity(temp)
-    console.log("zipcode here")
-    if (!(response===undefined)) {
+    let response = await getCity(zipCode)
+    if (!(response === undefined)) {
+      let curr = this.state.cityList;
+      for (let i = 0; i < response.data.length; i++) {
+        console.log("for loop")
+        console.log(response.data[i].City)
+        let temp = {
+          city: response.data[i].City,
+          state: response.data[i].State,
+          lat: response.data[i].Lat,
+          lon: response.data[i].Long,
+          population: response.data[i].EstimatedPopulation,
+          wages: response.data[i].TotalWages
+        }
+        console.log(temp.city)
+        curr = curr.concat(temp);
+        //console.log(this.cityList[i].state)
+      }
       this.setState({
-        city: response.data[0].City,
-        state: response.data[0].State,
-        lat: response.data[0].Lat,
-        lon: response.data[0].Long,
-        population: response.data[0].EstimatedPopulation,
-        wages: response.data[0].TotalWages,
+        cityList: curr,
         visible: true
-      })
+      });
+      console.log("length of cityList", this.state.cityList.length)
+      console.log("city info", this.state.cityList[2].state)
     }
     else {
       this.setState({
@@ -80,11 +93,14 @@ class SearchForm extends Component {
             <h1>{this.state.zipCode}</h1>
           </form>
           <div className="ResultsCard">
-            <h1> {this.state.city}, {this.state.state} </h1>
-            <h2> State: {this.state.state} </h2>
-            <h2> Location: {this.state.lat}, {this.state.lon} </h2>
-            <h2> Population (estimated): {this.state.population} </h2>
-            <h2> Total Wages: {this.state.wages}</h2>
+            {this.state.cityList.map(item => (
+              <React.Fragment key={item.id}>
+                <h2>City: {item.city}</h2>
+                <h3>State: {item.state}</h3>
+                <h3>Location: {item.lat}, {item.lon}</h3>
+                <h3>Wages: {item.wages}</h3>
+              </React.Fragment>
+            ))}
           </div>
         </div>
       );
@@ -106,4 +122,4 @@ class SearchForm extends Component {
   }
 }
 
-    export default SearchForm;
+export default SearchForm;
